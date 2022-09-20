dcb892fc7ed722187776b3d93b813e6968d6d025
Aniruddh Upparna <aniruddhupparna@gmail.com>
2022-09-20 13:16:41 +0530

    added basic websocket server for ui controlled direction events

---
+++ src/web_socket_server.py
@@ -0,0 +1,20 @@
+import eventlet
+import socketio
+
+sio = socketio.Server(cors_allowed_origins='*')
+app = socketio.WSGIApp(sio)
+
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
+
+@sio.event
+def message(sid, data):
+    print('message ', data)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('', 3000)), app)

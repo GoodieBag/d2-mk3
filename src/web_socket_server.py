e65c1a0c241cf6e3d35f42ec2e6746c26de575d0
Aniruddh Upparna <aniruddhupparna@gmail.com>
2022-09-24 22:03:52 +0530

    added change to test and  integrate movements with bot

--- src/web_socket_server.py
+++ src/web_socket_server.py
@@ -1,8 +1,21 @@
 import eventlet
 import socketio
+from lib.bot import Bot
 
 sio = socketio.Server(cors_allowed_origins='https://diggibot.surge.sh')
 app = socketio.WSGIApp(sio)
+
+bot = Bot()
+
+def handle_movements(command):
+    if command == 'forward':
+        bot.move_forwards()
+    if command == 'backward':
+        bot.move_backwards()
+    if command == 'left':
+        bot.turn_left()
+    if command == 'right':
+        bot.turn_right()
 
 @sio.event
 def connect(sid, environ):
@@ -11,6 +24,7 @@ def connect(sid, environ):
 @sio.event
 def message(sid, data):
     print('message ', data)
+    handle_movements(data)
 
 @sio.event
 def disconnect(sid):
